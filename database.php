bb046ddecfabaeeea98a8f8b51cc1cae090d0bb8
tamararosario <tamara.rosario@fwpr.com>
2017-06-12 13:39:16 +0200

    DB Update

--- database.php
+++ database.php
@@ -8,16 +8,18 @@ function pg_connection_string_from_database_url() {
     extract(parse_url($_ENV["DATABASE_URL"]));
     return "user=$user password=$pass host=$host dbname=$dbname"; # <- you may want to add sslmode=require there too
 }
+
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
+$acc = json_decode($_GET['accJSON']);
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
+$result = pg_query($pg_conn, "INSERT INTO accelerometer (x, y, z) VALUES ('$acc[0]','$acc[1]','$acc[2]')");
+/*print "<pre>\n";
 if (!pg_num_rows($result)) {
     print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
     print "Tables in your database:\n";
     while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
-print "\n";
+print "\n";*/
 ?>
