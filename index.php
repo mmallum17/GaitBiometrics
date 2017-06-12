079f6a9b5556218daad587beb6c5c9c1b9b8925c
Marcus <marcusmallum@gmail.com>
2017-06-12 13:46:55 +0200

    DB Update

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php
-//    header( 'Location: /index.html' ) ;
-
-function pg_connection_string_from_database_url() {
-    $user = "fnfgduxoqaonjm";
-    $pass = "3a5ef4d082610ea1f9f88d557ae503cb2cb7d5dd5eb97f5c9c0c23a7c0ddf15a";
-    $host = "ec2-23-21-220-48.compute-1.amazonaws.com";
-    $dbname = "d5vthvev6u48e9";
-    extract(parse_url($_ENV["DATABASE_URL"]));
-    return "user=$user password=$pass host=$host dbname=$dbname"; # <- you may want to add sslmode=require there too
-}
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-    print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-    print "Tables in your database:\n";
-    while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
+    header( 'Location: /index.html' ) ;
 ?>
