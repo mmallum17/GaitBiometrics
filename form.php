dd2f6e417a8106532b15a309e13d5fec80adc24c
Marcus <marcusmallum@gmail.com>
2017-06-16 17:52:08 +0200

    Redirect Form Page

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
-//header( 'Location: /LayoutApp/App.html' ) ;
+header( 'Location: /LayoutApp/App.html' ) ;
 function pg_connection_string_from_database_url() {
     $user = "fnfgduxoqaonjm";
     $pass = "3a5ef4d082610ea1f9f88d557ae503cb2cb7d5dd5eb97f5c9c0c23a7c0ddf15a";
