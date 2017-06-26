e09f4a4e1214d6a14191bcf1da7a63157985f119
Marcus <marcusmallum@gmail.com>
2017-06-27 01:01:10 +0200

    Test ID

--- form.php
+++ form.php
@@ -2,7 +2,7 @@
 //$uniqid()
 session_start();
 $userName = $_POST["Name"];
-header( "Location: /LayoutApp/audiotest.html" ) ;
+//header( "Location: /LayoutApp/audiotest.html" ) ;
 function pg_connection_string_from_database_url() {
     $user = "fnfgduxoqaonjm";
     $pass = "3a5ef4d082610ea1f9f88d557ae503cb2cb7d5dd5eb97f5c9c0c23a7c0ddf15a";
@@ -33,6 +33,7 @@ $result = pg_query($pg_conn, $query);
 //$query = "SELECT currval(pg_get_serial_sequence('user_info','id'))";
 //$result = pg_query($pg_conn, $query);
 $id = $result;
+print $id;
 if (!$result) {
     $errormessage = pg_last_error();
     echo "Error with query: " . $errormessage;
